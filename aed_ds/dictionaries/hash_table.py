fe0e400620355cd1ee0ee0480cc7682644816a4a
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-21 13:09:39 +0100

    Add insert to Hash Table.

--- aed_ds/dictionaries/hash_table.py
+++ aed_ds/dictionaries/hash_table.py
@@ -1,18 +1,42 @@
 from .tad_dictionary import Dictionary
 from ..exceptions import NoSuchElementException, DuplicatedKeyException
 from ..lists.singly_linked_list import SinglyLinkedList
+from .item import Item 
+
+import ctypes 
 
 class HashTable(Dictionary):
     def __init__(self, size=101):
-        pass
+        self.array_size = size
+        self.num_elements = 0
+        self.table = (self.array_size * ctypes.py_object)() # Array of pointers
 
-    def size(self): pass
+        # Create an empty list for each table position
+        for i in range(self.array_size):
+            self.table[i] = SinglyLinkedList()
 
-    def is_full(self): pass
+    def size(self):
+        return self.num_elements
+
+    def is_full(self):
+        return self.num_elements == self.array_size
 
     def get(self, k): pass
 
-    def insert(self, k, v): pass
+    def insert(self, k, v):
+        # Check if it has key
+        if self.has_key(k):
+            raise DuplicatedKeyException()
+
+        ## Insert new item
+        # Calculate the table index
+        idx = self.hash_function(k) # O(1)
+        # Create a new Item
+        item = Item(k, v)
+        # Insert the item in the colision list
+        self.table[idx].insert_last(item)
+        # Update the number of elements
+        self.num_elements += 1    
 
     def update(self, k, v): pass
 
@@ -23,3 +47,16 @@ class HashTable(Dictionary):
     def values(self): pass
 
     def items(self): pass
+
+    def hash_function(self, k):
+        return sum([ord(c) for c in k]) % self.array_size
+
+    def has_key(self, k):
+        idx = self.hash_function(k) # O(1)    
+        colision_list = self.table[idx]
+        it = colision_list.iterator()
+        while it.has_next():
+            current_item = it.next()
+            if current_item.get_key() == k:
+                return True
+        return False
