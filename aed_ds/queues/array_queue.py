126ff1da2bf055e92421de5d24ec0fe3fe1c9795
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-21 10:58:19 +0100

    Add TAD Stack and TAD Queue with stub implementation.

---
+++ aed_ds/queues/array_queue.py
@@ -0,0 +1,12 @@
+from .tad_queue import Queue
+
+class ArrayQueue(Queue):
+    def isEmpty(self): pass
+
+    def isFull(self): pass
+
+    def size(self): pass
+
+    def enqueue(self, element): pass
+
+    def dequeue(self): pass
