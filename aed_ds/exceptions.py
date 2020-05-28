43849dbac0a135e532e76ad548a5993c06dc3ebf
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-28 10:07:59 +0100

    Update exceptions.

--- aed_ds/exceptions.py
+++ aed_ds/exceptions.py
@@ -1,9 +1,30 @@
 class EmptyListException(Exception):
     pass
 
+
 class InvalidPositionException(Exception):
     pass
 
+
 class NoSuchElementException(Exception):
     pass
-    
+
+
+class EmptyStackException(Exception):
+    pass
+
+
+class FullStackException(Exception):
+    pass
+
+
+class EmptyQueueException(Exception):
+    pass
+
+
+class FullQueueException(Exception):
+    pass
+
+
+class DuplicatedKeyException(Exception):
+    pass
