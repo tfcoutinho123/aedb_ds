e1133ac5c2bcf2846953186b8e3579c7687aa3b3
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-28 10:12:26 +0100

    Fix method naming convetion on Stacks and Queueues.

--- aed_ds/queues/array_queue.py
+++ aed_ds/queues/array_queue.py
@@ -1,9 +1,9 @@
 from .tad_queue import Queue
 
 class ArrayQueue(Queue):
-    def isEmpty(self): pass
+    def is_empty(self): pass
 
-    def isFull(self): pass
+    def is_full(self): pass
 
     def size(self): pass
 
